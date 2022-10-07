0cec00b740d3d2975da1f66fa1036bf187302876
Mashayikh Shaikh <shakmash@gmail.com>
2022-10-07 13:04:19 +0530

    Synchronizing job Assignment_1 from AWS Glue at 2022/10/07 07:34:19

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -22,7 +22,7 @@ DataSource0 = glueContext.create_dynamic_frame.from_catalog(database = "assignme
 
 
 
-
+# Transformations
 Transform0 = ApplyMapping.apply(frame = DataSource0, mappings = [("serial number", "long", "serial number", "long"), ("company name", "string", "company name", "string"), ("employee markme", "string", "employee markme", "string"), ("leave", "long", "leave", "int")], transformation_ctx = "Transform0")
 
 final_df=Transform0.toDF().withColumn("Timestamp",F.current_timestamp())
@@ -33,11 +33,24 @@ df_with_year_and_month = (final_df
 
 
 df=DynamicFrame.fromDF(df_with_year_and_month,glueContext,"final_dynamic_frame")
+# End of transformations
 
+# DataSink0 = glueContext.write_dynamic_frame.from_options(frame =df , format_options = {"compression": "snappy"}, connection_type = "s3", format = "glueparquet", connection_options = {"path": "s3://testglue100/Output_files/Assignment_1/","partitionKeys":["year","month"]}, transformation_ctx = "DataSink0")
 
-DataSink0 = glueContext.write_dynamic_frame.from_options(frame =df , format_options = {"compression": "snappy"}, connection_type = "s3", format = "glueparquet", connection_options = {"path": "s3://testglue100/Output_files/Assignment_1/","partitionKeys":["year","month"]}, transformation_ctx = "DataSink0")
-
-
+AmazonS3_node1665051501748 = glueContext.getSink(
+    path="s3://testglue100/Output_files/Assignment-1_a",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=["year","month"],
+    compression="snappy",
+    enableUpdateCatalog=True,
+    transformation_ctx="partition_athena",
+)
+AmazonS3_node1665051501748.setCatalogInfo(
+    catalogDatabase="assignment 1", catalogTableName="Parquet_output"
+)
+AmazonS3_node1665051501748.setFormat("glueparquet")
+AmazonS3_node1665051501748.writeFrame(df)
 
 
 job.commit()
